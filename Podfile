5375ebef5881542349c1a31c26b0a01cee5128a1
George <george@immedia.co.za>
2017-01-04 08:26:57 +0200

    initial commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+
+target 'RainyShine' do
+  use_frameworks!
+
+pod 'Alamofire', '~> 4.0'
+
+end
---
+++ RainyShine/WeatherVC.swift
@@ -0,0 +1,49 @@
+//
+// WeatherVC.swift
+//  RainyShine
+//
+//  Created by George Kapoya on 2017/01/03.
+//  Copyright © 2017 immeddia. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	@IBOutlet weak var imgMain: UIImageView!
+	@IBOutlet weak var tableView: UITableView!
+
+	@IBOutlet weak var lblCondition: UILabel!
+	@IBOutlet weak var lblCity: UILabel!
+	@IBOutlet weak var lblTempreture: UILabel!
+	@IBOutlet weak var lblDate: UILabel!
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+
+	}
+
+
+
+
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 5
+	}
+
+	func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+		return 1
+	}
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+		return cell
+
+	}
+
+}
+
